71c84518e04ab8e627aed3e3a54d0a50bdee0142
Tran Son Hai <harefx.dev@gmail.com>
2021-03-14 18:45:12 +0700

    sample messages

---
+++ src/components/Messages.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h4>Messages</h4>
+    <ul v-for="(message, i) in messages" :key="i">
+      <li>{{ message }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Messages",
+  data() {
+    return {
+      messages: ["hello", "hi", "It's working"]
+    };
+  }
+};
+</script>
